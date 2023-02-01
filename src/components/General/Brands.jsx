ec937e80d55a13f43d55ddcebe4b8d650f8a8858
ghesshu <hughestotimee@gmail.com>
2023-02-01 21:32:20 +0000

    Brand Update

--- src/components/General/Brands.jsx
+++ src/components/General/Brands.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react';
 
 import Aos from 'aos'
 import client1 from '../../assets/client-1.png'
@@ -10,8 +10,11 @@ import client5 from '../../assets/client-5.png'
 import client6 from '../../assets/client-6.png'
 import client7 from '../../assets/client-7.png'
 import client8 from '../../assets/client-8.png'
+import './General.css'
 
 const Brands = () => {
+
+  
   useEffect(() => {
     Aos.init({duration: 1000});
   }, []);
@@ -19,7 +22,35 @@ const Brands = () => {
   return (
     <div data-aos="flip-up">
 
-<div className="flex object-contain justify-between flex-wrap">
+<div className="relative overflow-hidden p-16 xl:p-28 ">
+
+<div className="flex absolute left-0 animate justify-between">
+<div className="flex object-contain justify-evenly w-1/2">
+  <img src={client1} alt="" className='w-10 slg:w-16 xl:w-24 h-full' />
+  <img src={client2} alt="" className='w-10 slg:w-16 xl:w-24 h-full' />
+  <img src={client3} alt="" className='w-10 slg:w-16 xl:w-24 h-full' />
+  <img src={client4} alt="" className='w-10 slg:w-16 xl:w-24 h-full' />
+  <img src={client5} alt="" className='w-10 slg:w-16 xl:w-24 h-full' />
+  <img src={client6} alt="" className='w-10 slg:w-16 xl:w-24 h-full' />
+  <img src={client7} alt="" className='w-10 slg:w-16 xl:w-24 h-full' />
+  <img src={client8} alt="" className='w-10 slg:w-16 xl:w-24 h-full' />
+</div>
+
+<div className="flex object-contain justify-evenly w-1/2">
+  <img src={client1} alt="" className='w-10 slg:w-16 xl:w-24 h-full' />
+  <img src={client2} alt="" className='w-10 slg:w-16 xl:w-24 h-full' />
+  <img src={client3} alt="" className='w-10 slg:w-16 xl:w-24 h-full' />
+  <img src={client4} alt="" className='w-10 slg:w-16 xl:w-24 h-full' />
+  <img src={client5} alt="" className='w-10 slg:w-16 xl:w-24 h-full' />
+  <img src={client6} alt="" className='w-10 slg:w-16 xl:w-24 h-full' />
+  <img src={client7} alt="" className='w-10 slg:w-16 xl:w-24 h-full' />
+  <img src={client8} alt="" className='w-10 slg:w-16 xl:w-24 h-full' />
+</div>
+</div>
+
+</div>
+
+      {/* <div className="flex object-contain justify-between flex-wrap">
         <img src={client1} alt="" className='w-10 slg:w-16 xl:w-24 h-full' />
         <img src={client2} alt="" className='w-10 slg:w-16 xl:w-24 h-full' />
         <img src={client3} alt="" className='w-10 slg:w-16 xl:w-24 h-full' />
@@ -28,7 +59,7 @@ const Brands = () => {
         <img src={client6} alt="" className='w-10 slg:w-16 xl:w-24 h-full' />
         <img src={client7} alt="" className='w-10 slg:w-16 xl:w-24 h-full' />
         <img src={client8} alt="" className='w-10 slg:w-16 xl:w-24 h-full' />
-      </div>
+      </div> */}
       
     </div>
   )
